56690966474a4f8be2b55ba86bce6a41cda9ef9f
Lavam Yadav <yadav.lavam238@gmail.com>
2020-05-11 17:56:08 +0530

    fix

--- src/main/java/com/cg/fms/controller/FlightController.java
+++ src/main/java/com/cg/fms/controller/FlightController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.fms.dto.Flight;
@@ -18,6 +19,7 @@ import com.cg.fms.payload.RestResponse;
 import com.cg.fms.service.FlightServiceImpl;
 
 @RestController
+@RequestMapping("/flight")
 public class FlightController {
 
 	@Autowired
